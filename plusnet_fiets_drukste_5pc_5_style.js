0aa9a8bee1451e07e3fd77e38b40bdb4612b305d
csluijs <45770349+csluijs@users.noreply.github.com>
2022-06-17 14:31:44 +0200

    Add files via upload

---
+++ plusnet_fiets_drukste_5pc_5_style.js
@@ -0,0 +1,56 @@
+var size = 0;
+var placement = 'point';
+function categories_plusnet_fiets_drukste_5pc_5(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'drukste 5% met geen of smal fietspad':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(215,25,28,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'overig plusnet':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(109,109,109,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(44,238,177,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_plusnet_fiets_drukste_5pc_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("drukste_5pc");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_plusnet_fiets_drukste_5pc_5(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
---
+++ venstertijdgebieden_1_style.js
@@ -0,0 +1,96 @@
+var size = 0;
+var placement = 'point';
+function categories_venstertijdgebieden_1(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case 'eenrichtingsverkeer muv laden en lossen':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(171,221,164,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'eenrichtingsverkeer muv laden en lossen tijdens venstertijd':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(171,221,164,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'gesloten voor mvt muv laden en lossen':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(51,160,44,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'gesloten voor mvt muv laden en lossen tijdens venstertijd':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(51,160,44,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'in een richting verbod stil te staan muv laden en lossen tijdens venstertijd':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,127,0,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'verbod stil te staan':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(165,91,221,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case 'verbod stil te staan muv laden en lossen tijdens venstertijd':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(252,244,1,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+default:
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(233,23,90,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_venstertijdgebieden_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("laden_lossen");
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_venstertijdgebieden_1(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
